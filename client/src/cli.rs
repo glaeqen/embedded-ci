fcdea88322796e6b55911cd1b14d8a5ac048b504
Emil Fresk <emil.fresk@gmail.com>
2022-02-16 09:55:31 +0100

    Added more run options to client

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -1,11 +1,12 @@
 use clap::Parser;
+use embedded_ci_client::{ProbeAlias, ProbeSerial};
 use embedded_ci_server::{CpuId, RunOn, TargetName};
 use reqwest::Url;
 use std::{path::PathBuf, time::Duration};
 
 /// A thin CLI on top of the `embedded-ci-server` API calls
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version, about, long_about = None, group = clap::ArgGroup::new("options").multiple(false))]
 struct Args {
     /// Server address.
     #[clap(
@@ -20,11 +21,19 @@ struct Args {
     auth_token: Option<String>,
 
     /// The target on which to run.
-    #[clap(long)]
+    #[clap(long, group = "options")]
     target: Option<String>,
 
+    /// The probe alias on which to run.
+    #[clap(long, group = "options")]
+    probe_alias: Option<String>,
+
+    /// The probe serial on which to run.
+    #[clap(long, group = "options")]
+    probe_serial: Option<String>,
+
     /// The core on which to run.
-    #[clap(long)]
+    #[clap(long, group = "options")]
     core: Option<CpuId>,
 
     /// Timeout in seconds.
@@ -47,17 +56,12 @@ pub struct Cli {
 pub fn cli() -> Cli {
     let args = Args::parse();
 
-    let run_on = match (args.target, args.core) {
-        (Some(target), None) => RunOn::Target(TargetName(target)),
-        (None, Some(core)) => RunOn::Core(core),
-        (None, None) => {
-            println!("Error: Only one of --target or --core can be used at the same time");
-            std::process::exit(1);
-        }
-        (Some(_), Some(_)) => {
-            println!("Error: One of --target or --core is required");
-            std::process::exit(1);
-        }
+    let run_on = match (args.target, args.core, args.probe_alias, args.probe_serial) {
+        (Some(target), _, _, _) => RunOn::Target(TargetName(target)),
+        (_, Some(core), _, _) => RunOn::Core(core),
+        (_, _, Some(probe_alias), _) => RunOn::ProbeAlias(ProbeAlias(probe_alias)),
+        (_, _, _, Some(probe_serial)) => RunOn::ProbeSerial(ProbeSerial(probe_serial)),
+        _ => unreachable!(),
     };
 
     Cli {
